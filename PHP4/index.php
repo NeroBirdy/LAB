deb25deb371b3ae2d21362cd048cf51682630b92
NeroBirdy <nirvanav.39@gmail.com>
2024-02-04 13:37:58 +0500

    .

---
+++ PHP4/index.php
@@ -0,0 +1,50 @@
+<?php
+
+$link = mysqli_connect("127.0.0.1", "root", "", "php4");
+$test = mysqli_query($link, "SELECT * FROM images");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+</head>
+
+<body>
+    <header>
+        <a class="header" href="main.html">Главная</a>
+        <a class="header" href="contact.html">Контакты</a>
+        <a class="header" href="gallary.html">Галерея</a>
+    </header>
+    <div class="gal_content">
+        <div class="gal_main">
+            <?php
+            while ($result = mysqli_fetch_assoc($test)) 
+            {
+                $path = $result['path'];
+            ?>
+                <a data-fancybox="images" href=<?php echo $path ?>> <img src=<?php echo $path ?> class="gal_pic" /></a>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+    <footer>
+        <div class="footer_area">
+            <img src="pictures/back/stik.png" alt="" class="stik">
+        </div>
+        <div class="footer_info">
+            <p>Зорин Владислав</p>
+            <p>+7 (950) 522-89-97</p>
+            <p>31231@gmail.com</p>
+        </div>
+    </footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+
+</html>
