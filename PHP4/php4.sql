480990cd10f382952064f3f93773c5152ad1f496
NeroBirdy <nirvanav.39@gmail.com>
2024-02-05 09:25:29 +0500

    .

--- PHP4/php4.sql
+++ PHP4/php4.sql
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `images` (
   `id` int NOT NULL,
-  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `images`
