731cb7b79970ca6792193aa6b20d54765ddbf325
NeroBirdy <nirvanav.39@gmail.com>
2024-02-08 20:31:38 +0500

    .

--- PHP5/index.php
+++ PHP5/index.php
@@ -1,23 +1,26 @@
 <?php
 
-$link = mysqli_connect("127.0.0.1","root","","php5");
+$link = mysqli_connect("127.0.0.1", "root", "", "php5");
 $query = "SELECT * FROM comments";
-$stmt = mysqli_query($link,$query);
+$stmt = mysqli_query($link, $query);
+$monthes = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
-        <a class = "header" href="main.html">Главная</a>
-        <a class = "header" href="contact.html">Контакты</a>
-        <a class = "header" href="gallary.html">Галерея</a>
+        <a class="header" href="main.html">Главная</a>
+        <a class="header" href="contact.html">Контакты</a>
+        <a class="header" href="gallary.html">Галерея</a>
     </header>
     <div class="content">
         <div class="main_m">
@@ -33,8 +36,8 @@ $stmt = mysqli_query($link,$query);
         </div>
     </div>
     <div class="comments_area">
-            <div class="comments">
-            <form action="load.php" method="post" class = "form">
+        <div class="comments">
+            <form action="load.php" method="post" class="form">
                 <input type="text" name="user" placeholder="Пользователь">
                 <input type="text" name="comment" placeholder="Комментарий">
                 <button type="submit">Отправить</button>
@@ -43,20 +46,34 @@ $stmt = mysqli_query($link,$query);
             while ($result = mysqli_fetch_assoc($stmt)) {
                 $user = $result['user'];
                 $comment = $result['comment'];
+                $timestamp = $result['date'];
+                $pieces = explode(" ", $timestamp);
+                $date = $pieces[0];
+                $tmp = explode("-", $date);
+                $stringMonth = $tmp[1];
+                $stringDay = $tmp[2];
+                $month = $monthes[intval($date[0])];
+                $day = intval($stringDay);
+                $trueDate = $day . " " . $month;
             ?>
-            <div class = "comment"><p class = "user"><?php echo $user ?></b> 
-            <p><?php echo $comment ?></p></div>
+                <div class="comment">
+                    <p class="date"><?php echo $trueDate ?></p>
+                    <div class="comment_info">
+                        <p class="user"><?php echo $user ?>
+                        <p><?php echo $comment ?></p>
+                    </div>
+                </div>
             <?php
             }
             ?>
-            </div>
         </div>
+    </div>
     <footer>
         <div class="upper">
-            <div class = "footer_area">
-                <img src="pictures/back/stik.png" alt="" class = "stik">
+            <div class="footer_area">
+                <img src="pictures/back/stik.png" alt="" class="stik">
             </div>
-            <div class = "footer_info">
+            <div class="footer_info">
                 <p>Зорин Владислав</p>
                 <p>+7 (950) 522-89-97</p>
                 <p>31231@gmail.com</p>
@@ -64,4 +81,5 @@ $stmt = mysqli_query($link,$query);
         </div>
     </footer>
 </body>
+
 </html>
--- PHP6/index.php
+++ PHP6/index.php
@@ -3,7 +3,7 @@
 $link = mysqli_connect("127.0.0.1", "root", "", "php5");
 $query = "SELECT * FROM comments";
 $stmt = mysqli_query($link, $query);
-
+$monthes = ["December","January","February","March","April","May","June","July","August","September","October","November"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,10 +46,20 @@ $stmt = mysqli_query($link, $query);
                 $user = $result['user'];
                 $comment = $result['comment'];
                 $id = $result['id'];
+                $timestamp = $result['date'];
+                $pieces = explode(" ", $timestamp);
+                $date = $pieces[0];
+                $tmp = explode("-",$date);
+                $stringMonth = $tmp[1];
+                $stringDay = $tmp[2];
+                $month = $monthes[intval($date[0])];
+                $day = intval($stringDay);
+                $trueDate = $day . " " . $month;
             ?>
                 <div class="comment">
+                    <p class="date"><?php echo $trueDate ?></p>
                     <div class="comment_info">
-                        <p class="user"><?php echo $user ?></b>
+                        <p class="user"><?php echo $user ?>
                         <p><?php echo $comment ?></p>
                     </div>
                     <div class="change">
